0eff53208ec02a4f9b229d5f3c644d0b2ac2f63d
jiachiun <jiachiun@gmail.com>
2013-09-07 19:20:12 +0800

    UI Draft for MainPage

---
+++ Perks/Perks/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Perks.Resources;
+
+namespace Perks
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Perks/Perks/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.Phone.Controls;
+
+
+namespace Perks
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnDirections.Tap += BtnDirectionsOnTap;
+
+        }
+
+
+        private void BtnDirectionsOnTap(object sender, GestureEventArgs gestureEventArgs)
+        {
+
+        }
+
+
+        private void appBarBtnRefresh_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void appBarMenuItemAbout_Click(object sender, System.EventArgs e)
+        {
+
+
+        }
+
+        private void appBarBtnInfo_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
